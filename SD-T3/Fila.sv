2587dae3791d628c81f857db6b7e3f12fe7f26ad
e-pietro <eduardoopietro05@gmail.com>
2025-05-23 09:24:59 -0300

    Update Fila.sv

--- SD-T3/Fila.sv
+++ SD-T3/Fila.sv
@@ -12,16 +12,60 @@ aparecer em data_out no ciclo subsequente se o número de elementos (len_out) fo
 */
 
 module Fila(
-    input logic data_in,
+    input logic [7:0] data_in,
     input loigc enqueue_in,
     input logic reset,
     input logic dequeue_in,
-    input logic clock_10khz
-    output logic len_out,
-    output logic data_out
+    input logic clock_10khz,
+    output logic [7:0] len_out,
+    output logic [7:0] data_out
 );
 
-    //registrador
-    logic [7:0] queue
+    //FSM
+    typedef enum logic [2:0] {
+    enqueue = 3'b000,
+    dequeue = 3'b001,
+    ack = 3'b010
+    } state_t;
+
+    //Registradores
+    state_t state;
+    state_t prev_state;
+    logic element_count;
+    logic head;
+
+    /*
+        enqueue:
+            bota na primeira posição se estiver vazia
+            se não coloca atrás do último colocado
+
+        dequeue:
+            não acontece nada se estiver vazia
+            tira o primeiro elemento e o elemento de trás vira o head
+    */
+
+   funciton logic [7:0] enqueue ()
+
+   always_ff @(posedge clock or posedge reset ) begin
+    if (reset) begin
+        data_in <= 8'b0;
+        data_out <= 8'b0;
+        enqueue_in <= 1'b0;
+        clock_10khz <= 1'b0;
+        len_out <= 8'b0;
+        dequeue_in <= 1'b0;
+    end
+
+    case (state)
+        enqueue: begin 
+            if (element_count == 0) head <= data_in
+            else 
+        
+        end
+
+    end
+
+
+
     
 
