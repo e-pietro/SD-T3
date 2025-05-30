8f91089875c03b1c732cc5b9750659e40a0f7b4d
GabrielDCW <163864472+GabrielDCW@users.noreply.github.com>
2025-05-30 08:56:19 -0300

    Update top.sv
    top

--- SD-T3/top.sv
+++ SD-T3/top.sv
@@ -1,1 +1,30 @@
+module top(
+  input logic reset,
+  input logic clock,
+  input logic data_in,
+  output logic [7:0] buffer,
+  output logic data_ready,
+);
+
+  Deserializador dut(
+    .clk(clk),
+    .reset(reset),
+    .data_in(data_in),
+    .write_in(write_in),
+    .ack_in(ack_in),
+    .data_out(data_out),
+    .data_ready(data_ready),
+    .status_out(status_out)
+  );
+
+  Fila dut(
+    .data_in(data_in),
+    .enqueue_in(enqueue_in),
+    .reset(reset),
+    .dequeue_in(dequeue_in),
+    .clock_10khz(clock_10khz),
+    .len_out(len_out),
+    .data_out(dataout)
+  );
+      
 
